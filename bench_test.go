4bc3c0c54a9a52ca1a1c360ad6b8cc586d52e863
KyleBanks <kyle@nthgensoftware.com>
2017-03-14 09:15:39 -0400

    Moved benchmark test out of its own package

--- bench_test.go
+++ bench_test.go
@@ -1,35 +1,33 @@
-package benchmark
+package depth
 
 import (
 	"testing"
-
-	"github.com/KyleBanks/depth"
 )
 
 func BenchmarkTree_ResolveStrings(b *testing.B) {
-	benchmarkTreeResolveStrings(&depth.Tree{}, b)
+	benchmarkTreeResolveStrings(&Tree{}, b)
 }
 
 func BenchmarkTree_ResolveStringsInternal(b *testing.B) {
-	benchmarkTreeResolveStrings(&depth.Tree{
+	benchmarkTreeResolveStrings(&Tree{
 		ResolveInternal: true,
 	}, b)
 }
 
 func BenchmarkTree_ResolveStringsTest(b *testing.B) {
-	benchmarkTreeResolveStrings(&depth.Tree{
+	benchmarkTreeResolveStrings(&Tree{
 		ResolveTest: true,
 	}, b)
 }
 
 func BenchmarkTree_ResolveStringsInternalTest(b *testing.B) {
-	benchmarkTreeResolveStrings(&depth.Tree{
+	benchmarkTreeResolveStrings(&Tree{
 		ResolveInternal: true,
 		ResolveTest:     true,
 	}, b)
 }
 
-func benchmarkTreeResolveStrings(t *depth.Tree, b *testing.B) {
+func benchmarkTreeResolveStrings(t *Tree, b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		if err := t.Resolve("strings"); err != nil {
 			b.Fatal(err)
