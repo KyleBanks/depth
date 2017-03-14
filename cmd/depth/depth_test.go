05c0242aed347a3c697ee9056db94efc2d8619ec
KyleBanks <kyle@nthgensoftware.com>
2017-03-14 13:31:24 -0400

    Improving test coverage

--- cmd/depth/depth_test.go
+++ cmd/depth/depth_test.go
@@ -37,10 +37,66 @@ func Example_handlePkgsTestStrings() {
 	//   └ unsafe
 }
 
+func Example_handlePkgsDepth() {
+	var t depth.Tree
+
+	handlePkgs(&t, []string{"github.com/KyleBanks/depth/cmd/depth"})
+	// Output:
+	// github.com/KyleBanks/depth/cmd/depth
+	//   ├ encoding/json
+	//   ├ flag
+	//   ├ fmt
+	//   ├ io
+	//   ├ os
+	//   ├ strings
+	//   └ github.com/KyleBanks/depth
+	//     ├ bytes
+	//     ├ errors
+	//     ├ go/build
+	//     ├ os
+	//     ├ path
+	//     ├ sort
+	//     └ strings
+}
+
 func Example_handlePkgsUnknown() {
 	var t depth.Tree
 
 	handlePkgs(&t, []string{"notreal"})
 	// Output:
 	// 'notreal': FATAL: unable to resolve root package
+}
+
+func Example_handlePkgsJson() {
+	outputJSON = true
+	var t depth.Tree
+	handlePkgs(&t, []string{"strings"})
+
+	// Output:
+	// {
+	//   "name": "strings",
+	//   "resolved": true,
+	//   "deps": [
+	//     {
+	//       "name": "errors",
+	//       "resolved": true,
+	//       "deps": null
+	//     },
+	//     {
+	//       "name": "io",
+	//       "resolved": true,
+	//       "deps": null
+	//     },
+	//     {
+	//       "name": "unicode",
+	//       "resolved": true,
+	//       "deps": null
+	//     },
+	//     {
+	//       "name": "unicode/utf8",
+	//       "resolved": true,
+	//       "deps": null
+	//     }
+	//   ]
+	// }
 }
